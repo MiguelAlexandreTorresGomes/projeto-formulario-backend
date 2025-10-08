3b3d850666e92521a24748124290425efe79a66a
MiguelAlexandreTorresGomes <miguelatg74@gmail.com>
2025-10-08 13:59:48 -0300

    fix: remove registeradmin

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@ import { registerAdmin,login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/registerAdmin", registerAdmin); 
+// router.post("/registerAdmin", registerAdmin); 
 router.post("/login", login);                 
 
 export default router;
